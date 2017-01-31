214f4561be6fd429d3c64ef3ca51205aeae2af5a
Yoan Blanc <yoan@dosimple.ch>
2017-01-31 10:19:40 +0100

    Add basic Game model.
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- tsuro/apps/games/admin.py
+++ tsuro/apps/games/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Game
+
 # Register your models here.
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'uuid')
--- tsuro/apps/games/models.py
+++ tsuro/apps/games/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+import uuid as uuidlib
 
-# Create your models here.
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+from djchoices import DjangoChoices, ChoiceItem
+
+
+class Game(TimeStampedModel):
+    """A Game between some participants."""
+
+    # Choices
+    class GameType(DjangoChoices):
+        Open = ChoiceItem("O")
+        Running = ChoiceItem("R")
+        Unfinished = ChoiceItem("U")
+        Done = ChoiceItem("D")
+
+    # Fields
+    uuid = models.UUIDField(
+        db_index=True, default=uuidlib.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    type = models.CharField(
+        max_length=1,
+        choices=GameType.choices,
+        validators=[GameType.validator])
+
+    def __str__(self):
+        return "{0.title}".format(self)
