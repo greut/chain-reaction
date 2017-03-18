facd4daa16772ee4510845a1907a195de8304c75
Yoan Blanc <yoan@dosimple.ch>
2017-03-18 17:00:01 +0100

    deployment script.
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -xe
+
+. ~/.bash_profile
+cd www/app
+. bin/activate
+export DJANGO_SETTINGS_MODULE=chain_reaction.settings.production
+git pull
+python manage.py migrate
+python manage.py collectstatic --noinput
+sudo sv restart uwsgi
+sudo sv reload nginx
