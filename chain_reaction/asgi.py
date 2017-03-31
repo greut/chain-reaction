5f619379a4691a241ba8f08f8df1bfb46997013d
Yoan Blanc <yoan@dosimple.ch>
2017-03-31 11:34:17 +0200

    Force asgi in production mode by default (for runit)
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- chain_reaction/asgi.py
+++ chain_reaction/asgi.py
@@ -1,6 +1,7 @@
 import os
 from channels.asgi import get_channel_layer
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chain_reaction.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "chain_reaction.settings.production")
 
 channel_layer = get_channel_layer()
